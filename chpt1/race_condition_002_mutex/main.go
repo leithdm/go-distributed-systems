7b8621cfbd30b35ab8596732c6c07cbe02e5757e
leithdm <leithdm@users.noreply.github.com>
2025-04-19 16:20:34 +0100

    add mutex example

---
+++ chpt1/race_condition_002_mutex/main.go
@@ -0,0 +1,54 @@
+package main
+
+// Example: Data Race Condition without Mutex
+// Counter value is not predictable, and is not 1000
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Example: Data Race Condition without Mutex
+func noMutex() {
+	var counter int
+	var wg sync.WaitGroup
+
+	// Increment counter concurrently.
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			counter++ // Data race occurs here.
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter value:", counter)
+
+}
+
+// Example: Data Race Condition with Mutex
+// Counter value is predictable, and always is 1000
+func muTex() {
+	var counter int
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	// Safely increment counter using a mutex.
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			mu.Lock()
+			counter++
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("Counter value:", counter)
+}
+
+func main() {
+	muTex()
+}
